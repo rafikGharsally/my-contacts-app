d0c586b9b0a59416d5304325b872bc7220526ad4
rafikgharsally <time2dream@live.fr>
2018-06-01 14:43:57 -0400

    make it work with  Redux :)
    make the contacts app great again -_-

--- src/index.js
+++ src/index.js
@@ -1,8 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { Provider } from 'react-redux';
+import { ConnectedRouter } from 'react-router-redux';
+import createHistory from 'history/createBrowserHistory';
 import registerServiceWorker from './registerServiceWorker';
+import './index.css';
+import App from './containers/App';
+import createReduxStore from './store/configureStore';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootEl = document.getElementById('root');
+
+const history = createHistory();
+const newStore = createReduxStore();
+
+const renderApp = (Component) => {
+  console.log('histroy', history);
+  ReactDOM.render(
+    <Provider store={newStore}>
+      <ConnectedRouter history={history}>
+        <Component/>
+      </ConnectedRouter>
+    </Provider>,
+    rootEl
+  );
+};
+
+renderApp(App);
+
+if (module.hot) {
+  module.hot.accept('./containers/App', () => {
+    const NextApp = require('./containers/App').default;
+    renderApp(NextApp);
+  })
+}
+
 registerServiceWorker();
