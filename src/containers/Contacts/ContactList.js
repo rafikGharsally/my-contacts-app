12498505ef4c661ede90f0e40757b9dadf6a10c7
rafikgharsally <time2dream@live.fr>
2018-06-02 18:36:23 -0400

    init contacts component

---
+++ src/containers/Contacts/ContactList.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+
+class ContactList extends Component {
+
+
+  render() {
+
+    return (
+      <div>teststtst contacts list</div>
+    );
+  }
+}
+
+
+
+export default ContactList;
