ef7c88994d796ca9e02b4d62e91d05d91f0187dd
rafikgharsally <time2dream@live.fr>
2018-06-02 18:36:01 -0400

    added new route for contacts

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
+import ContactsList from '../Contacts';
+
 import '../../App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      test
+        <Switch>
+          <Route exact path="/" component={ContactsList} />
+          <Route render={() => (
+            <h2>Page not found Error 404</h2>
+            )} />
+        </Switch>
       </div>
     );
   }
