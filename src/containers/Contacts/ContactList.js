a63894d191889d4dd2356ae710860065a8b82286
rafikgharsally <time2dream@live.fr>
2018-06-02 22:47:32 -0400

    created container for contacts

--- src/containers/Contacts/ContactList.js
+++ src/containers/Contacts/ContactList.js
@@ -1,17 +1,38 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { get } from 'axios';
+import { fetchContacts } from '../../actions/Contacts';
+
 
 
 class ContactList extends Component {
+
+  componentDidMount() {
+    fetchContacts();
+  }
 
 
   render() {
 
     return (
-      <div>teststtst contacts list</div>
+      <div>contacts list</div>
     );
   }
 }
 
+const mapDispatchToProps = dispatch => {
+  return bindActionCreators({ fetchContacts }, dispatch);
+};
+
+const mapStateToProps = state => {
+  console.log('state---' ,state);
+  return {
+    contacts: {}
+  }
+};
 
 
-export default ContactList;
+export default connect(
+  mapStateToProps, mapDispatchToProps)(ContactList);
+
