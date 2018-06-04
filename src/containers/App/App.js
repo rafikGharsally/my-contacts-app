3d473a91b1be2b54be00eaed03b63442b033d4d3
rafikgharsally <time2dream@live.fr>
2018-06-04 16:44:41 -0400

    added new route to edit an existing contact

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -2,19 +2,15 @@ import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import ContactsList from '../Contacts';
 import NewContact from '../NewContact';
+import EditContact from '../EditContact';
 import Grid from '@material-ui/core/Grid';
 import Sidebar from '../Sidebar';
-
-
-
 import '../../App.css';
 
 const fullHeight = {
   height: '100%',
   position: 'relative'
 };
-
-
 
 class App extends Component {
   render() {
@@ -27,15 +23,13 @@ class App extends Component {
             <Switch>
               <Route exact path="/" component={ContactsList} />
               <Route exact path="/new" component={NewContact} />
+              <Route exact path="/edit/:id" component={EditContact} />
               <Route render={() => (
                 <h2>Page Not Found Error 404</h2>
               )} />
             </Switch>
           </Grid>
         </Grid>
-
-
-
       </div>
 
     );
