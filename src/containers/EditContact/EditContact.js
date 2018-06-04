c9ace58feeff472ca78ff6421f012161dc062028
rafikgharsally <time2dream@live.fr>
2018-06-04 17:40:40 -0400

    display current contact to edit :baby:  baaam

--- src/containers/EditContact/EditContact.js
+++ src/containers/EditContact/EditContact.js
@@ -1,16 +1,43 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { getContact, modifyContact } from '../../actions/contact';
+import { AddContact, ContactForm } from '../../components/GenericPages/Contact';
 
 
 class EditContact extends Component {
 
+  componentDidMount() {
+    const { getContact, match: { params } } = this.props;
+    getContact(params.id);
+  }
+
   render() {
 
+    const { contact: { data } } = this.props;
     return (
-      <div>sjjsghshgsh</div>
+
+      <AddContact title="Edit contact" previousState="/">
+        <ContactForm
+          contact={data}
+          submitForm={this.submit}
+        />
+      </AddContact>
 
     );
   }
 }
 
+const mapStateToProps = state => {
+  return {
+    contact: state.contact
+  }
+};
 
-export default EditContact;
+const mapDispatchToProps = dispatch => {
+  return bindActionCreators({ getContact, modifyContact }, dispatch);
+};
+
+export default connect(
+  mapStateToProps, mapDispatchToProps)(EditContact);
+
