d0ffefcd7dd5ca7623e32923448e17ecae0865ec
rafikgharsally <time2dream@live.fr>
2018-06-04 16:44:57 -0400

    added edit contact component

---
+++ src/containers/EditContact/EditContact.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+
+class EditContact extends Component {
+
+  render() {
+
+    return (
+      <div>sjjsghshgsh</div>
+
+    );
+  }
+}
+
+
+export default EditContact;
