9385b8d25ced44221173e3cc876fa67f0d78f720
rafikgharsally <time2dream@live.fr>
2018-06-04 10:46:15 -0400

    :happy: call get contacts and  display contact list coming from api

--- src/containers/Contacts/ContactList.js
+++ src/containers/Contacts/ContactList.js
@@ -9,14 +9,27 @@ import { fetchContacts } from '../../actions/Contacts';
 class ContactList extends Component {
 
   componentDidMount() {
-    fetchContacts();
+   this.props.fetchContacts();
   }
 
 
   render() {
-
+    const { contacts } = this.props;
+    const notFound = !contacts.data ||  contacts.data.length < 0;
     return (
-      <div>contacts list</div>
+      <div>
+        <h2>Contact List</h2>
+        { contacts.data && contacts.data.length > 0 && contacts.data.map((contact) => {
+          return (
+            <div key={contact.id}>{contact.name}</div>
+          )
+        })
+        }
+        { notFound &&
+          <h3>no contacts to display...</h3>
+        }
+      </div>
+
     );
   }
 }
@@ -25,10 +38,11 @@ const mapDispatchToProps = dispatch => {
   return bindActionCreators({ fetchContacts }, dispatch);
 };
 
+
 const mapStateToProps = state => {
   console.log('state---' ,state);
   return {
-    contacts: {}
+    contacts: state.contacts
   }
 };
 
